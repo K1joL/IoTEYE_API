5a110c9da1e22654a863a30dd85eb2259e4c9c79
Bogdan Shults <kzk19@mail.ru>
2023-12-28 13:27:50 +0300

    Returning to a multi-file project

---
+++ Headers/export.h
@@ -0,0 +1,10 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#ifndef IOTEYELIB_STATIC_DEFINE
+#  include <ioteyelib/export_shared.h>
+#else
+#  include <ioteyelib/export_static.h>
+#endif
+
+#endif //!EXPORT_H
--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
-#define CLIENT_API_MODE
-#define DEBUG
 const char* USERID = "";
+#include "Headers/IoTEYE_API.h"
 
-#include "Sources/IoTEYE_API.hpp"
+#include <thread>
 
 int main()
 {
     // Передаем номер метода POST и сформированный пакет
-    USERID = ioteyeApi::RegisterNewUser("Kijol").c_str();
-    std::cout << USERID << std::endl;
-    ioteyeApi::CreateVirtualPin("0", "int", "199");
-    std::cout << ioteyeApi::GetVirtualPin("0") << std::endl;
-    std::cout << ioteyeApi::UpdateVirtualPin("0","1") << std::endl;
-    std::cout << ioteyeApi::GetVirtualPin("0") << std::endl;
-    std::cout << ioteyeApi::DeleteVirtualPin("0") << std::endl;
-    std::cout << ioteyeApi::GetVirtualPin("0") << std::endl;
-
-    // cpr::Payload p{{"userID", "kukaID"}};
-    // p.Add({"cmd", "pv"});
-    // p.Add({"pinNumber", "0"});
-    // p.Add({"dataType", "int"});
-    // p.Add({"value", "0"});
+    // USERID = ioteyeApi::RegisterNewUser("kijol").c_str();
+    std::cout << ioteyeApi::RegisterNewUser("kijol") << std::endl;
+    // ioteyeApi::CreateVirtualPin("0", "int", "199");
+    // std::cout << ioteyeApi::GetVirtualPin("0") << std::endl;
+    // std::cout << ioteyeApi::UpdateVirtualPin("0","1") << std::endl;
+    // std::cout << ioteyeApi::GetVirtualPin("0") << std::endl;
+    // std::cout << ioteyeApi::DeleteVirtualPin("0") << std::endl;
+    // std::cout << ioteyeApi::GetVirtualPin("0") << std::endl;
+    ioteyeApi::RegisterNewDevice("device1");
+    ioteyeApi::GetDeviceStatus("device1");
+    cpr::Payload p{{"cmd","us"}};
+    char key;
+    while(true)
+    {
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        ioteyeApi::SendRequest(ioteyeApi::GET, p, "/user/kijol/devices/1/us");
+    }
     return 0;
 }
